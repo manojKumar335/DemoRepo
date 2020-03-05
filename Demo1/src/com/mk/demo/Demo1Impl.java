bc9c8bb11556c63512e3e3a88c9f014d65e1aaf4
Kapil <Kapil@Kapil-PC>
2020-03-05 09:36:32 +0530

    ewfrf

---
+++ Demo1/src/com/mk/demo/Demo1Impl.java
@@ -0,0 +1,14 @@
+package com.mk.demo;
+
+public class Demo1Impl extends Demo2 implements Demo1 {
+    
+	public static  void m2() {
+		
+
+	}
+	public static void main(String[] args) {
+		Demo1 d=new  Demo1Impl();
+		Demo1Impl d2=new Demo1Impl();
+		
+	}
+}
